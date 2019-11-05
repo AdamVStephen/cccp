babba3f1b7c2fb08ebe291279e7dfd6acf200073
Adam V Stephen (ukaea-fpga) <adam.v.stephen@gmail.com>
2019-11-05 08:45:17 +0000

    Add matplotlib plotting and larger array sin/cos demo.

--- cpp/G4SerDes/G4SerDes.cpp
+++ cpp/G4SerDes/G4SerDes.cpp
@@ -15,13 +15,28 @@
 G4int depoEnergyTime[7][11] = {{0}};
 G4int reacEnergyTime[7][11] = {{0}};
 
+
+#define G4_ARRAY_SIZE 5001
+#define G4_ARRAY_WRITE_SIZE (G4_ARRAY_SIZE - 1)
+
+G4int g4Sin[7][G4_ARRAY_SIZE] = {{0}};
+G4int g4Cos[7][G4_ARRAY_SIZE] = {{0}};
+
+
 void g4sdInit() {
 
   for(int i=0; i<7; i++) {
+
     for(int j=0; j<11; j++) {
       depoEnergyTime[i][j] = i*100 + j;
       reacEnergyTime[i][j] = i*1000 + j;
     }
+
+    for(int t=0;t<5000;t++) {
+      g4Sin[i][t] = (i+1) * sin((i+1)*M_PI*t/180);
+      g4Cos[i][t] = (i+1) * cos((i+1)*M_PI*t/180);
+    }
+      
   }
   return;
 }
@@ -41,8 +56,9 @@ void g4sdShow() {
 }
 
 bool g4sdSerialise(const char *filename, int asize) {
+
   std::ofstream fout;
-  std::cout << "g4sdSerialise to g4arrays.dat" << std::endl;
+  std::cout << "g4sdSerialise short arrays to g4arrays.{10,11}.dat" << std::endl;
 
   G4int gEventNumber = 42;
   std::string name = "neutron";
@@ -59,6 +75,25 @@ bool g4sdSerialise(const char *filename, int asize) {
   }
 
   fout.close();
+
+  // Oscillatory waveforms to help test plotting routines in python
+
+  std::string waveforms = "g4.waves.dat";
+
+  fout.open(waveforms,
+	    std::ios::binary | std::ios::out | std::ios::trunc);
+
+  for(int i=0; i<7; i++) {
+    fout << "\n" << std::setw(5) << gEventNumber << " EventId: " << name << " Type: " << i << "\n";
+    fout.write(reinterpret_cast<char*>(g4Sin[i]),
+	       std::streamsize(G4_ARRAY_WRITE_SIZE * sizeof(G4int)));
+    fout.write(reinterpret_cast<char*>(g4Cos[i]),
+	       std::streamsize(G4_ARRAY_WRITE_SIZE * sizeof(G4int)));
+  }
+
+  fout.close();
+
+
   return true;
 }
 
--- python/G4SerDes.py
+++ python/G4SerDes.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 import pdb
 import re
 import struct
 
 headerPat = """.*([0-9]+) EventId: ([a-z]+) Type: ([0-9]+)"""
 
-def g4Des(datafile, arrayDim1, arrayDim2, elemSizeInBytes):
+def g4Read(datafile, arrayDim1, arrayDim2, elemSizeInBytes):
     headers = []
     depoEnergyTime = []
     reacEnergyTime = []
@@ -26,19 +31,54 @@ def g4Des(datafile, arrayDim1, arrayDim2, elemSizeInBytes):
     return (headers, depoEnergyTime, reacEnergyTime)
         
 
-def main():
+def g4Show():
     for (filename, dim1, dim2, elemSize) in [
             ['g4arrays.10.dat', 7, 10, 4],
             ['g4arrays.11.dat', 7, 11, 4]]:
         print('%'*40)
         print('Array size %d' % dim2)
         print('%'*40)
-        headers, depoEnergyTime,reacEnergyTime = g4Des(filename, dim1, dim2, elemSize)
+        headers, depoEnergyTime,reacEnergyTime = g4Read(filename, dim1, dim2, elemSize)
         for idx, det in enumerate(depoEnergyTime):
             print('%-35s : %s' % (headers[idx].strip(), det))
         print('-'*80)
         for idx, ret in enumerate(reacEnergyTime):
             print('%-35s : %s' % (headers[idx].strip(), ret))
+
+
+def g4Plot(writePdf = False, pdfFileName = 'g4.waves.pdf'):
+    with PdfPages(pdfFileName) as pdf:
+        (headers, g4sines, g4cosines) = g4Read('g4.waves.dat', 7, 5000, 4)
+        for (row, sin) in enumerate(g4sines):
+            t = np.arange(len(sin))
+            nsin = np.array(sin)
+            plt.subplot(7,2,(2+2*row))
+            plt.plot(t,nsin)
+            plt.ylabel('g4 Sin %d' % row)
+            plt.xlabel('t')
+        for (row, cos) in enumerate(g4cosines):
+            t = np.arange(len(cos))
+            nsin = np.array(cos)
+            plt.subplot(7,2,(1+2*row))
+            plt.plot(t,nsin)
+            plt.ylabel('g4 Cos %d' % row)
+            plt.xlabel('t')
+        if writePdf:
+            pdf.savefig()
+            print("Saved plots to %s" % pdfFileName)
+        else:
+            plt.show()
+        pdf.savefig()
+        # Metadata for fun
+        d = pdf.infodict()
+        d['Title'] = 'cccp matplotlib demo of subplot with G4 style data'
+        d['Author'] = 'Adam Vercingetorix Stephen'
+        d['CreationDate'] = datetime.datetime.today()
+        
+    
+def main():
+    g4Show()
+    g4Plot(True)
     
 if __name__ == '__main__':
     main()
